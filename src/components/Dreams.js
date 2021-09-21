77fda97d8a23135b21045e53b8bf7198d2926971
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-09-21 08:56:01 -0700

    update Dreams.js
    Co-authored-by: professional-pigeon <professional-pigeon@users.noreply.github.com>

--- src/components/Dreams.js
+++ src/components/Dreams.js
@@ -1,42 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Button } from "react-bootstrap"
-import { v4 } from 'uuid';
-import { useSelector } from 'react-redux'
-import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase'
-import Dream from './Dream'
-
-function Dreams(props) {
-  useFirestoreConnect([
-    { collection: 'dreams' }
-  ])
-  const dreams = useSelector(state => state.firestore.ordered.dreams);
-  if (isLoaded(dreams)) {
-    return (
-      <React.Fragment>
-        {dreams.map((dream) => {
-          return <Dream
-          
-            />
-        })}
-      </React.Fragment>
-    )
-  }
-
-
-  return(
-    <div>
-      <p>This is a stand in for the things that will populate when you click a person.</p>
-    </div>
-  )
-
-}
-
-export default Dreams;
-
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Button } from "react-bootstrap"
 import { useSelector } from 'react-redux'
 import { useFirestoreConnect, useFirestore, isLoaded, isEmpty } from 'react-redux-firebase'
 import Dream from './Dream'
